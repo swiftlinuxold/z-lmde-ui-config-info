0d6cf9888dccccead0b2c8f2c9c209055e978fef
Jason Hsu <github@jasonhsu.com>
2012-04-08 12:18:04 -0500

    Removed unused function

--- main.py
+++ main.py
@@ -30,37 +30,6 @@ print '======================================'
 print 'BEGIN ADDING SYSTEM INFORMATION WIZARD'
 
 import shutil, subprocess
-
-def install_pkg_antix (name1, name2, url):
-    # First check for package
-    # Credit: http://stackoverflow.com/questions/3387961/check-if-a-package-is-installed
-    devnull = open (os.devnull,"w")
-    retval = subprocess.call(["dpkg","-s",name1],stdout=devnull,stderr=subprocess.STDOUT)
-    devnull.close()
-    i = 0
-    while (retval <> 0) and (i < 50):
-        os.system ('echo DOWNLOADING ' + name1 + ' FROM ' + url)
-        wget_command = 'wget -nv -nd -nH -r -l1 -q --no-parent -A '
-        deb_file = name1 + '_*' + name2
-        wget_command = wget_command + chr(39) + deb_file + chr(39) + ' '
-        wget_command = wget_command + url
-        os.system ('echo ' + wget_command)
-        os.system (wget_command)
-        os.system ('dpkg -i ' + deb_file)
-        os.system ('rm ' + deb_file)
-        os.system ('rm robot*')
-        devnull = open (os.devnull,"w")
-        retval = subprocess.call(["dpkg","-s",name1],stdout=devnull,stderr=subprocess.STDOUT)
-        devnull.close()
-        if (i > 3) and (retval <> 0):
-            os.system ('echo Installation not completed, will try again')
-            import time, random
-            sec = random.randrange (3,10)
-            time.sleep (sec)
-    if retval == 0:
-        os.system ('echo ' + name1 + ' is already installed')
-    else:
-        os.system ('echo WARNING: ' + name1 + ' is NOT installed')
 	
 src = dir_develop + '/ui-config-info/usr_local_bin/config-info.py'
 dest = '/usr/local/bin/config-info.py'
